a5b367550ac1cb570f7491ef7b96c41b37cf497e
cicigirl1 <cidaliamagule8@gmail.com>
2024-12-15 11:29:26 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,22 +8,3 @@ darkModeIcone.onclick = () => {
     document.body.classList.toggle('dark-mode')
 };
 
-<script>
-    document.getElementById
-  document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e. preventDefault();
-    const name = document.getElementById('fuul name').value.trim();
-    const mobile = document.getElementById('mobile number').value.trim();
-    const email = document.getElementById('emaill address').value.trim();
-    const subject = document.getElementById('emaill subject').value.trim();
-    const message = document.getElementById('your message').value.trim();
-
-    if (!name || !mobile || !email || !subject || !message) {
-      e. preventDefault();
-      alert('Por favor, preencha todos os campos obrigatórios.');
-     } else { 
-        alert('Formulario enviado com sucesso');
-        this. submit();
-    }
-  });
-</script>
